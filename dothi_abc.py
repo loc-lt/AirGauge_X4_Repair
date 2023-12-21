93e2654c90b7f902c553eede3967932f1ea3aa89
Le Thanh Loc <lochcmus@gmail.com>
2023-12-21 15:10:55 +0700

    add data today (3 con hang)

--- dothi_abc.py
+++ dothi_abc.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-with open("data20231213_133557.txt",'r') as filename:
+with open("data20231221_144049.txt",'r') as filename:
     txt = filename.read()
 list_total = txt.split()
 list_a = []
@@ -27,16 +27,20 @@ min_b = [11.11]*len(list_b)
 max_b = [11.15]*len(list_b)
 thresh_b = [11.16]*len(list_b)
 
+# Comment qua lại 2 cụm code dưới để theo dõi dữ liệu kênh A và kênh B
 
-plt.plot(x_axis, list_a)
-plt.plot(x_axis, min_a)
-plt.plot(x_axis, max_a)
-plt.plot(x_axis, thresh_a)
+# # Kênh A để đọc dữ liệu từ Ring (sẽ phải đo 3 lần), mỗi lần cách nhau chưa rõ ràng, cũng ko có cách nào rõ biệt nhận
+# plt.plot(x_axis, list_a)
+# plt.plot(x_axis, min_a)
+# plt.plot(x_axis, max_a)
+# plt.plot(x_axis, thresh_a)
 
-# plt.plot(x_axis, list_b[0:len(list_a)])
-# plt.plot(x_axis, min_b)
-# plt.plot(x_axis, max_b)
-# plt.plot(x_axis, thresh_b)
+# Kênh B để đọc dữ liệu từ Ping (đo 2 lần, mỗi lần đọc cách nhau 1 lần rút con hàng BW Mini ra khỏi Ping)
+# Tức là mình sẽ nhận biết 2 lần đọc khi đường kính nó quay lại  trên / dưới Threshold  
+plt.plot(x_axis, list_b[0:len(list_a)])
+plt.plot(x_axis, min_b)
+plt.plot(x_axis, max_b)
+plt.plot(x_axis, thresh_b)
 
 plt.title('title name')
 plt.xlabel('x_axis name')
